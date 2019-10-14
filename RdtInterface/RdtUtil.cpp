2ca175132fad70c973ae964e08ff4eab45ca77f1
madhav <shanbhagm@gmail.com>
2019-10-14 15:30:37 +0530

    added extra files to cmakelist

--- RdtInterface/RdtUtil.cpp
+++ RdtInterface/RdtUtil.cpp
@@ -3,9 +3,9 @@
 
 SequenceFlow::SequenceFlow(string s) {
 	mString = s;
-	LOGD("Entering: %s", mString.c_str());
+	LOGD("Entering: %s\n", mString.c_str());
 }
 
  SequenceFlow::~SequenceFlow() {
-	LOGD("Exiting: %s", mString.c_str());
+	LOGD("Exiting: %s\n", mString.c_str());
 }
