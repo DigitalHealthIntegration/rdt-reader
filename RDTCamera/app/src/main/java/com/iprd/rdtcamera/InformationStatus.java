f32abfc4732cf4086170499822995491fd82ae44
madhav <shanbhagm@gmail.com>
2019-11-15 12:43:33 +0530

    Image test to run on all images in a folder

---
+++ RDTCamera/app/src/main/java/com/iprd/rdtcamera/InformationStatus.java
@@ -0,0 +1,11 @@
+package com.iprd.rdtcamera;
+
+import android.graphics.Bitmap;
+
+public class InformationStatus{
+    public Bitmap mTrackedImage;
+
+    public InformationStatus(){
+        mTrackedImage=null;
+    }
+}
