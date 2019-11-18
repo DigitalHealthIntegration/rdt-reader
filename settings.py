d409fc261544d24e66a5b43c0f8a1109bd0e5b9e
Kashyap Jois <kjois@iprdgroup.com>
2019-11-18 19:43:09 +0530

    Added new settings to enable changing between different models

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 RDT_GIT_ROOT="D:\\source\\repos\\rdt-reader"
-FLU_AUDERE_PATH="D:\\source\\repos\\rdt-reader\\tensorflow-yolov3-models\\models\\Flu_audere\\1"
-FLU_AUDERE_LINE_PATH="D:\\source\\repos\\rdt-reader\\tensorflow-yolov3-models\\models\\Flu_audere_line\\1"
-YOLO_MODEL_VER         = 1
+FLU_AUDERE_PATH="D:\\source\\repos\\rdt-reader\\tensorflow-yolov3-models\\models\\Flu_audere\\2"
+FLU_AUDERE_LINE_PATH="D:\\source\\repos\\rdt-reader\\tensorflow-yolov3-models\\models\\Flu_audere_line\\3"
+YOLO_MODEL_VER         = 2
+LINE_MODEL_VER         = 3
