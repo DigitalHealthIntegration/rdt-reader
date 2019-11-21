aff0085ab2a62a3a4b91042147cca9324cfbdb8b
kashyap jois <kjois@iprdgroup.com>
2019-11-21 16:47:39 +0530

    Path changes for my local system Warning

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-RDT_GIT_ROOT="D:\\source\\repos\\rdt-reader"
-FLU_AUDERE_PATH="D:\\source\\repos\\rdt-reader\\tensorflow-yolov3-models\\models\\Flu_audere\\2"
-FLU_AUDERE_LINE_PATH="D:\\source\\repos\\rdt-reader\\tensorflow-yolov3-models\\models\\Flu_audere_line\\3"
+RDT_GIT_ROOT="C:\\Users\\Kashyap\\bkp\\source\\repos\\rdt-reader"
+FLU_AUDERE_PATH="C:\\Users\\Kashyap\\bkp\\source\\repos\\rdt-reader\\tensorflow-yolov3-models\\models\\Flu_audere\\2"
+FLU_AUDERE_LINE_PATH="C:\\Users\\Kashyap\\bkp\\source\\repos\\rdt-reader\\tensorflow-yolov3-models\\models\\Flu_audere_line\\3"
 YOLO_MODEL_VER         = 2
 LINE_MODEL_VER         = 3
