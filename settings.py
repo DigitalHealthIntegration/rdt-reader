153a32ea02349dcf1379d46ebad4f1d959c4161c
kashyapj2793 <ubuntu@ip-172-30-2-116.ec2.internal>
2020-01-16 16:01:50 +0000

    Changed paths for docker

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-RDT_GIT_ROOT="C:/Users/Kashyap/bkp/source/repos/rdt-reader"
-FLU_AUDERE_PATH="C:/Users/Kashyap/bkp/source/repos/rdt-reader/tensorflow-yolov3-models/models/Flu_audere/1"
-FLU_AUDERE_LINE_PATH="C:/Users/Kashyap/bkp/source/repos/rdt-reader/tensorflow-yolov3-models/models/Flu_audere_line/23" #19 trained with faint red
+RDT_GIT_ROOT="/home/rdtreader/rdt-reader"
+FLU_AUDERE_PATH="/home/rdtreader/rdt-reader/tensorflow-yolov3-models/models/Flu_audere/1"
+FLU_AUDERE_LINE_PATH="/home/rdtreader/rdt-reader/tensorflow-yolov3-models/models/Flu_audere_line/23" #19 trained with faint red
 YOLO_MODEL_VER         = 1
 LINE_MODEL_VER         = 8
